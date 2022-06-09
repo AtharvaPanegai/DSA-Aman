87903a1dc77a833189e1a94af5858f259ad29d8b
AtharvaPanegai <panegai.atharv@gmail.com>
2022-06-09 16:11:48 +0530

    Height Balanced Tree

---
+++ Tree/BinaryTreeQues/SumRootToLeafNumbers/program.cpp
@@ -0,0 +1,28 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    // constructor
+    TreeNode(int data)
+    {
+        val = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int sumNumbers(TreeNode *root)
+{
+    
+}
+
+int main()
+{
+
+    return 0;
+}

762c235c00d65d88339ea44de3324f61648e589d
AtharvaPanegai <panegai.atharv@gmail.com>
2022-06-09 10:07:56 +0530

    PreOrder Traversal

---
+++ Tree/TreeTraversals/PreOrderTraversals/program.cpp
@@ -0,0 +1,41 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    // constructor
+    TreeNode(int data)
+    {
+        val = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void preorder(TreeNode *root, vector<int> ans)
+{
+    if (!root)
+    {
+        return;
+    }
+    ans.push_back(root->val);
+    preorder(root->left, ans);
+    preorder(root->right, ans);
+}
+
+vector<int> preorderTraversal(TreeNode *root)
+{
+    vector<int> ans;
+    preorder(root, ans);
+    return ans;
+}
+
+int main()
+{
+
+    return 0;
+}

4bd89eda2179e07574aeec40e19b703fcf3bc26b
AtharvaPanegai <panegai.atharv@gmail.com>
2022-06-09 14:15:31 +0530

    Diameter of BT

--- Tree/BinaryTreeQues/DiameterOfBT/program.cpp
+++ Tree/BinaryTreeQues/DiameterOfBT/program.cpp
@@ -16,6 +16,27 @@ struct TreeNode
     }
 };
 
+int rec(TreeNode *root, int &d)
+{
+    if (!root)
+        return 0;
+
+    int ld = rec(root->left, d);
+    int rd = rec(root->right, d);
+
+    d = max(d, ld + rd);
+    return max(ld, rd) + 1;
+}
+
+int diameterTree(TreeNode *root)
+{
+    if (!root)
+        return 0;
+    int d = 0;
+    rec(root, d);
+    return d;
+}
+
 int main()
 {
 

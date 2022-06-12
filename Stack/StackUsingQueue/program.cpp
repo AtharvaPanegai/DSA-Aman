42c356047a17821928d83b3de2c300b558d596a1
AtharvaPanegai <panegai.atharv@gmail.com>
2022-06-12 11:44:06 +0530

    Stack using Queue

--- Stack/StackUsingQueue/program.cpp
+++ Stack/StackUsingQueue/program.cpp
@@ -4,62 +4,35 @@ using namespace std;
 class Stack
 {
     queue<int> q1;
-    queue<int> q2;
-    int N;
 
 public:
     Stack()
     {
-        N = 0;
     }
 
     void pop()
     {
-        if (!q1.empty())
-        {
-            return;
-        }
-
-        while (q1.size() != 1)
-        {
-            q2.push(q1.front());
-            q1.pop();
-        }
         q1.pop();
-        N--;
-
-        queue<int> temp = q1;
-        q1 = q2;
-        q2 = temp;
     }
 
     void push(int val)
     {
         q1.push(val);
-        N++;
+        for (int i = 0; i < q1.size() - 1; i++)
+        {
+            q1.push(q1.front());
+            q1.pop();
+        }
     }
 
     int top()
     {
-        if (q1.empty())
-        {
-            return -1;
-        }
+        return q1.front();
+    }
 
-        while (q1.size() == 1)
-        {
-            q2.push(q1.front());
-            q1.pop();
-        }
-
-        int ans = q1.front();
-        q2.push(ans);
-
-        queue<int> temp = q1;
-        q1 = q2;
-        q2 = temp;
-
-        return ans;
+    bool empty()
+    {
+        return q1.empty();
     }
 };
 

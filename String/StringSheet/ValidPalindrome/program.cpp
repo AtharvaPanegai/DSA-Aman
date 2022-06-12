b3b672f59840975cc1aa3858ff80f64e0ba8cb4a
AtharvaPanegai <panegai.atharv@gmail.com>
2022-06-12 13:06:32 +0530

    Valid Anagram

--- String/StringSheet/ValidPalindrome/program.cpp
+++ String/StringSheet/ValidPalindrome/program.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 bool isPalindrome(string s)
 {
-    for (int i = 0, j = s.size() - 1; i < s.size(), i < j; i++, j--)
+    for (int i = 0, j = s.size() - 1; i < j; i++, j--)
     {
-        while (!isalnum(s[i]))
+        while (isalnum(s[i]) == false && i < j)
             i++;
-        while (isalnum(s[j]))
+        while (isalnum(s[j]) == false && i < j)
             j--;
         if (toupper(s[i]) != toupper(s[j]))
             return false;
